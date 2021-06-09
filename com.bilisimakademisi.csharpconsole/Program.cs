490843bee35ab8cb78f24969abaef6a87f30de3c
Bilisim AKADEMISI25 <bilisim.akademisi25@arnavutkoy.local>
2021-06-09 18:47:58 +0300

    Proje dosyası ekle.

---
+++ com.bilisimakademisi.csharpconsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using com.bilisimakademisi.csharp.console;
+using System;
+
+namespace com.bilisimakademisi.csharpconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            User _user = new User();
+
+            _user.UserId = 1;
+            _user.fullName = "vildan.akin";
+
+            Console.WriteLine(_user.fullName);
+
+        }
+    }
+     
+}
---
+++ com.bilisimakademisi.csharpconsole/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.bilisimakademisi.csharp.console
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string fullName { get; set; }
+
+    }
+}
